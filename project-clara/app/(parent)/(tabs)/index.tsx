7588564250123570d1816bbd6edcfd3d186b9387
jgallagherunt <201396242+jgallagherunt@users.noreply.github.com>
2025-10-05 21:22:34 -0500

    beginning of Card Component

--- project-clara/app/(parent)/(tabs)/index.tsx
+++ project-clara/app/(parent)/(tabs)/index.tsx
@@ -1,16 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { StyleSheet, View } from "react-native";
+
+import Card from "@/components/Card";
 
 export default function Index() {
+    const router = useRouter();
+    
+    const onCardPress = () => {
+        {/* test function with preset link, real cards will have link dynamically provided */}
+        router.push("/(parent)/(tabs)/live-updates");
+    };
 
-  return (
-    <View style={styles.container}>
-      <Text>This is a placeholder!</Text>
-    </View>
-  );
+
+    return (
+        <View style={styles.container}>
+            <Card header="Clickable Test Card" preview="Hello! I am testing this forever!! AAAAAAAAAAAA AHH AHH AHH IM BURNING" onPress={onCardPress}/>
+            <Card header="Test Card" preview="Hello! I am testing this forever!! AAAAAAAwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwAAAAA AHH AHH AHH IM BURNING"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+        alignContent: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+    },
 });

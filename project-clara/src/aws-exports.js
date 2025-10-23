fde01e06c0f438ba1a29a5462c4cc6f177b7c0dd
MajorAbeer <myfirsttax@gmail.com>
2025-10-23 09:03:21 -0500

    Add Amplify Cognito configuration to aws-exports.js

---
+++ project-clara/src/aws-exports.js
@@ -0,0 +1,10 @@
+const awsconfig = {
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_4PIyHXV77',
+    userPoolWebClientId: '23iuf0lrsg4e1truak7qhr46mt',
+    mandatorySignIn: true,
+  }
+};
+
+export default awsconfig;

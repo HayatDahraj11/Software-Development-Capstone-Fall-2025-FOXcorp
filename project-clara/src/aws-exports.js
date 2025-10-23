00cdb04f79a4ef5162eb20f7d16ead7f25b09a95
abeeruddin <165880964+abeeruddin@users.noreply.github.com>
2025-10-23 10:44:24 -0500

    Update aws-exports.js
    changed client ID

--- project-clara/src/aws-exports.js
+++ project-clara/src/aws-exports.js
@@ -2,7 +2,7 @@ const awsconfig = {
   Auth: {
     region: 'us-east-1',
     userPoolId: 'us-east-1_4PIyHXV77',
-    userPoolWebClientId: '23iuf0lrsg4e1truak7qhr46mt',
+    userPoolWebClientId: '3uh0uogbiksicgn7pknj1tk8g6',
     mandatorySignIn: true,
   }
 };

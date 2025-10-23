4a6e08268bf06efcc5df78163e5673e10516b185
jgallagherunt <201396242+jgallagherunt@users.noreply.github.com>
2025-10-23 12:49:19 -0500

    began work on general info screen
    creating dictionaries which store information on all the parent's children, and a dictionary which holds information about the parents, including an array of references to their children.
    will use this for the dropdown that lets parents see info on specific children

--- project-clara/app/(parent)/(tabs)/general-info.tsx
+++ project-clara/app/(parent)/(tabs)/general-info.tsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+// children dictionaries, which store key: value information about each child of the parent who is logged on
+// intended to be provided by database later, hardcoded for now
+const firstChildInfoDictionary = {
+    name: "Darcey",
+    classes: ["English", "Maths", "Mario"],
+}
+const secondChildInfoDictionary = {
+  name: "Daan",
+  classes: ["History", "Maths"],
+}
+
+// user info dictionary, or, all info about the parent that should be readily accessible
+// childern key holds array of dictionaries for each child
+const userInfoDictionary = {
+  children: [firstChildInfoDictionary, secondChildInfoDictionary]
+}
+
 export default function ParentGeneralInfoScreen() {
+
+  // this holds which child of the parent's is currently being displayed
+  const [childSelected, setChildSelected] = useState(userInfoDictionary.children[0]);
 
   return (
     <View style={styles.container}>

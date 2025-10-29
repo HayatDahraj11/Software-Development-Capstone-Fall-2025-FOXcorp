1322913d4de7184a160893aef7cbcd0e34fb9e7b
jgallagherunt <201396242+jgallagherunt@users.noreply.github.com>
2025-10-28 22:36:51 -0500

    Added a new Theme to the Card Component
    Added both a new theme to the card component, "list", aswell as the "theme?" property.
    This new theme displays the card with smaller text and a more compact form, aswell as only displaying the preview if "urgent? = true".
    This theme of card is meant to mimic the options in a hamburger menu.
    
    I also added a list of cards to the Records page in Student General Info

--- project-clara/app/(parent)/(tabs)/[studentId]/studentRecords.tsx
+++ project-clara/app/(parent)/(tabs)/[studentId]/studentRecords.tsx
@@ -1,16 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { useLocalSearchParams, useRouter, Href } from "expo-router";
+
+import Card from "@/components/Card";
 
 export default function StudentRecordsScreen() {
+    const { studentId } = useLocalSearchParams();
+    const router = useRouter();
 
-  return (
-    <View style={styles.container}>
-      <Text>This is a placeholder!</Text>
-    </View>
-  );
+    const RouteCard = (route: string): void => {
+            // if card has a route, use it. if not, ignore it
+            if(route !== " ") { 
+                router.push( (route) as Href );
+            }
+            else { }
+    };
+
+    return (
+        <View style={styles.container}>
+            <ScrollView contentContainerStyle={styles.listContainer}>
+                <Card header="Attendance" preview="" onPress={() => RouteCard(" ")} theme="list"/>
+                <Card header="Medical" preview="School needs updated vaccine records for your child!" onPress={() => RouteCard(" ")} theme="list" urgent={true}/>
+                <Card header="Transcript" preview="" onPress={() => RouteCard(" ")} theme="list"/>
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  listContainer: {
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+        marginTop: 10,
+    },
 });

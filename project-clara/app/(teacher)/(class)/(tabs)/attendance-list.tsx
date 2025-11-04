ddb6bd719cadd3cb1c7e0538d660b22098054e82
Douglas Borovina <douglasborovina@gmail.com>
2025-11-04 10:11:49 -0600

    Reapply "Reapply "Merge branch 'teacher-views'""
    This reverts commit e80d94a98c00a8d95cd7ab21a977bbf3193e2e25.

--- project-clara/app/(teacher)/(class)/(tabs)/attendance-list.tsx
+++ project-clara/app/(teacher)/(class)/(tabs)/attendance-list.tsx
@@ -1,13 +1,105 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Href, useRouter } from "expo-router";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import Card from "@/components/Card";
 
 export default function attendanceList() {
+  const router = useRouter();
 
-  return (
-    <View style={styles.container}>
-      <Text>This is a placeholder!</Text>
-    </View>
-  );
+  const CardFlatListData = [
+        {
+            id: 1,
+            header: 'Clickable Test Card',
+            preview: 'Student 1',
+            route: '/(teacher)/(class)/modal',
+        },
+        {
+            id: 2,
+            header: 'Clickable Test Card',
+            preview: 'Student 2',
+            route: ' '
+        },
+        {
+            id: 3,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 4,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 5,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 6,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 7,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 8,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 9,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 10,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+        {
+            id: 11,
+            header: 'Test Card',
+            preview: 'I am not clickable!',
+            route: ' ',
+        },
+    ];
+
+    const RouteCard = (route: string): void => {
+            // if card has a route, use it. if not, ignore it
+            if(route !== " ") {
+                router.push( (route) as Href );
+            }
+            else { }
+        };
+
+    return (
+            <View style={styles.container}>
+                <FlatList
+                    data={CardFlatListData}
+                    renderItem={({item, index}) => (
+                        <Card
+                            header={item.header}
+                            preview={item.preview}
+                            onPress={() => RouteCard(item.route)}
+                        />
+                    )}
+                />
+            </View>
+        );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {

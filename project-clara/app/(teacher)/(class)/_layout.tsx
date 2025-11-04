a274d5c6cb3fd08e09a43d1c7da2fa6e856c37d7
Douglas Borovina <douglasborovina@gmail.com>
2025-11-04 10:17:44 -0600

    Updated routing

--- project-clara/app/(teacher)/(class)/_layout.tsx
+++ project-clara/app/(teacher)/(class)/_layout.tsx
@@ -3,15 +3,13 @@ import { Stack } from "expo-router";
 export default function ClassLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* This includes your tab-based screens */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false,  contentStyle: { backgroundColor: "transparent" },}}  />
 
-      {/* This defines your modal presentation */}
       <Stack.Screen
         name="modal"
         options={{
           presentation: "transparentModal",
-          headerShown: true,
+          headerShown: false,
           title: "Modal",
         }}
       />
--- project-clara/app/index.tsx
+++ project-clara/app/index.tsx
@@ -1,8 +1,51 @@
-// In app/index.tsx
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-import { Redirect } from 'expo-router';
+import { Colors } from "@/constants/theme";
 
-export default function RootIndex() {
-  //  I Changed this to point to the school selection screen, as the initial screen
-  return <Redirect href="/login/school-selection" />;
+export default function Index() {
+
+  return (
+    <View style={styles.container}>
+      <Link href="/(parent)/(tabs)" asChild>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.pressableLabel}>Parent View</Text>
+        </Pressable>
+      </Link>
+      <Link href="/(teacher)/(class)/(tabs)/class" asChild>
+      <Pressable style={styles.pressable}>
+        <Text style={styles.pressableLabel}>Teacher View</Text>
+      </Pressable>
+      </Link>
+      <Pressable style={styles.pressable}>
+        <Text style={styles.pressableLabel}>Admin View</Text>
+      </Pressable>
+      <Link href="/_login" asChild>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.pressableLabel}>Login Screen</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pressableLabel: {
+    color: Colors.light.text,
+    textDecorationLine: 'underline',
+    fontSize: 16,
+  },
+  pressable: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+});

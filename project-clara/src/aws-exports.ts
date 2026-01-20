95f0ebecd6413a2daca228d8b2fdd26a61631b2f
jgallagherunt <201396242+jgallagherunt@users.noreply.github.com>
2026-01-20 12:06:44 -0600

    Reapply "Merge pull request #15 from HayatDahraj11/feat/connect-school-data"
    This reverts commit 3f00f28a5a655fcc8f1ca5d5f5cef6e340b71ec5.
    
    Reverting accidental reversion to SchoolPicker AWS integratio

--- project-clara/src/aws-exports.ts
+++ project-clara/src/aws-exports.ts
@@ -1,15 +1,20 @@
 // src/aws-exports.ts
-
 const awsconfig = {
   Auth: {
-    Cognito: { // ALL Cognito config goes inside here
-      region: 'us-east-1',
+    Cognito: {
       userPoolId: 'us-east-1_4PIyHXV77',
       userPoolClientId: '3uh0uogbiksicgn7pknj1tk8g6',
-      
+      region: 'us-east-1',
+    }
+  },
+  API: {
+    GraphQL: {
+      endpoint: 'https://4swn2sx4znarpjr56xia3nlqba.appsync-api.us-east-1.amazonaws.com/graphql',
+      region: 'us-east-1',
+      defaultAuthMode: 'apiKey',
+      apiKey: 'da2-qeswxhbdxvdhnn2xdupi7aduuy'
     }
   }
-  // Add other Amplify categories like API, Storage if needed later
 };
 
 export default awsconfig;

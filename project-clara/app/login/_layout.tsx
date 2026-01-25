bae990365c914088fef16655b38980a28e8418e4
jgallagherunt <201396242+jgallagherunt@users.noreply.github.com>
2026-01-25 16:28:17 -0600

    updating SchoolSelection screen to use ui theme

--- project-clara/app/login/_layout.tsx
+++ project-clara/app/login/_layout.tsx
@@ -1,11 +1,18 @@
+import { useThemeColor } from "@/src/features/app-themes/logic/use-theme-color";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
 
   return (
     <>
-      <Stack>
+      <Stack screenOptions={{
+        headerStyle: {
+          backgroundColor: useThemeColor({},"headerBackground")
+        },
+        headerTitleStyle: {
+          color: useThemeColor({},"text")
+        }
+      }}>
         <Stack.Screen name="index" options={{title: "Login"}}/>
         <Stack.Screen name="school-selection" options={{title: "School Selection"}}/>
       </Stack>

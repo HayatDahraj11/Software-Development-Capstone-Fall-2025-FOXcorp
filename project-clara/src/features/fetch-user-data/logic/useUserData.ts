f1d470dc8e7ef21fbeb3574cb17889fc8e7280ce
jgallagherunt <201396242+jgallagherunt@users.noreply.github.com>
2026-02-08 15:34:21 -0600

    useUserData hook with function for handling parent info

---
+++ project-clara/src/features/fetch-user-data/logic/useUserData.ts
@@ -0,0 +1,46 @@
+import { useCallback, useState } from "react";
+import { fetchParentWithKids, Parent, Student } from "../api/parent_data_fetcher";
+
+
+interface UseUserDataReturn {
+    isLoading: boolean; // true while functions are doing work and should not be interrupted
+    parent: Parent | undefined;
+    students: Student[] | undefined;
+    handleParentAndStudentData: () => Promise<void>;
+}
+
+export function useUserData(): UseUserDataReturn {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [parent, setParent] = useState<Parent>();
+    const [students, setStudents] = useState<Student[]>();
+
+    const handleParentAndStudentData = useCallback(async () => {
+        setIsLoading(true);
+
+        try {
+            const data = await fetchParentWithKids();
+            if(data.success) {
+                throw new Error(data.message);
+            } else if(data.parent && data.students) {
+                setParent(data.parent);
+                setStudents(data.students);
+                console.log("Parent and student data grabbed and saved successfully!");
+            } else {
+                throw new Error("Somehow, data came back as a success with no parent or student data attached?");
+            }
+
+        } catch(e) {
+            const err = e as {name?: string, message?: string};
+            console.error("useUserData.ts, handleParentAndStudentData: ", err.message);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [])
+
+    return {
+        isLoading,
+        parent,
+        students,
+        handleParentAndStudentData,
+    }
+}
